eadb8912c8269dfc7c0c0a5dc8172476f8801a86
shinokun <hypatia.tile02021@gmail.com>
2025-11-20 17:39:24 +0900

    Extract Neovim configuration into separate repo

--- .config/nvim/ftplugin/markdown.lua
+++
@@ -1,5 +0,0 @@
-vim.b.autoformat = false
-vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
-vim.opt_local.spell = true
-vim.opt_local.conceallevel = 2
--- .config/nvim/lua/shino/commands.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_user_command("InitLua", function()
-  vim.cmd.edit(vim.fn.stdpath "config" .. "/init.lua")
-  vim.notify("Open init.lua", vim.log.levels.INFO)
-end, {
-  desc = "Open init.lua",
-})
