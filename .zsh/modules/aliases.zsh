dd74be8dbc72acc38cc095e754cc53f6f5b45e60
shinokun <hypatia.tile02021@gmail.com>
2026-02-15 17:35:41 +0900

    add alias for 'fd' and 'bat' commands

--- .zsh/modules/aliases.zsh
+++ .zsh/modules/aliases.zsh
@@ -4,3 +4,5 @@
 
 alias ll="ls -lah"
 alias code='open -a /Applications/Visual\ Studio\ Code.app'
+alias fd='fdfind' # For systems where 'fd' is installed as 'fdfind'
+alias bat='batcat' # For systems where 'bat' is installed as 'batcat'
